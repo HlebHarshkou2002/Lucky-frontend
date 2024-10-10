078f5aa1de0cd260236a3c014e195de3a0ddc92a
unknown <erichkrause2012@tut.by>
2024-10-10 21:11:33 +0300

    added a switcher to categories menu on main page

--- src/Components/Intro/Categories/Categories.jsx
+++ src/Components/Intro/Categories/Categories.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import s from "./Categories.module.scss";
 import MenuBurger from "../../MenuBurger/MenuBurger";
 import Category from "./Category/Category";
@@ -14,23 +14,35 @@ import LawIcon from "../../../images/Categories/law.png";
 
 
 function Categories() {
+  const [isCategoriesMenuOpen, setIsCategoriesMenuOpen] = useState(true);
+
   return (
     <div className={s.categories__block}>
       <div className={s.categories__title__wrapper}>
         <div className={s.menu__burger__wrapper}>
-          <MenuBurger isWhite={true} />
+          {
+            isCategoriesMenuOpen ?
+              <MenuBurger isWhite={true} onClick={() => setIsCategoriesMenuOpen(!isCategoriesMenuOpen)} />
+              :
+              <div className={s.hide__menu} onClick={() => setIsCategoriesMenuOpen(!isCategoriesMenuOpen)}>X</div>
+          }
         </div>
         <span>Categories</span>
       </div>
 
-      <div className={s.categories__wrapper}>
-        <Category title={"Science"} icon={ScienceIcon}/>
-        <Category title={"Programming"} icon={ProgrammingIcon}/>
-        <Category title={"Business"} icon={BusinessIcon}/>
-        <Category title={"Design and Art"} icon={ArtIcon}/>
-        <Category title={"Languages"} icon={LanguagesIcon}/>
-        <Category title={"Law"} icon={LawIcon}/>
-      </div>
+      {isCategoriesMenuOpen ?
+        <div className={s.categories__wrapper}>
+          <Category title={"Science"} icon={ScienceIcon} />
+          <Category title={"Programming"} icon={ProgrammingIcon} />
+          <Category title={"Business"} icon={BusinessIcon} />
+          <Category title={"Design and Art"} icon={ArtIcon} />
+          <Category title={"Languages"} icon={LanguagesIcon} />
+          <Category title={"Law"} icon={LawIcon} />
+        </div>
+        :
+        <div></div>
+      }
+
     </div>
   );
 }
